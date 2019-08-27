c00717b066e1e9fdb22ac53b525d1272e258497d
Amanda MacGregor <amanda.macgregor13@gmail.com>
2019-08-27 14:32:54 -0700

    dashboard built - will be mobbing on refactor next

--- app/controllers/reu_program/dashboard_controller.rb
+++ app/controllers/reu_program/dashboard_controller.rb
@@ -2,23 +2,25 @@ module ReuProgram
   class DashboardController < AdminController
     def index
 
-      @sum = Applicant.count
-      @countStarted = Applicant.where(state: "Started").count
-      @countCompleted = Applicant.where(state: "Completed").count
-      @countAccepted = Applicant.where(state: "Accepted").count
-      @countRejected = Applicant.where(state: "Rejected").count
-      @countWithdrawn = Applicant.where(state: "Withdrawn").count
 
-      # @appStart = Setting.where(name: "Application Start").value
-      @values = Setting.first(6)
+      @applicants = Applicant.all
+      
+      @sum = Applicant.count
+      @count_started = Applicant.where(state: "Started").count
+      @count_completed = Applicant.where(state: "Completed").count
+      @count_accepted = Applicant.where(state: "Accepted").count
+      @count_rejected = Applicant.where(state: "Rejected").count
+      @count_withdrawn = Applicant.where(state: "Withdrawn").count
+
       @settings = Setting.all
-      # @value = Setting.important_field_path('value')
-      # @appDeadline = 
-      # @notDate = 
-      # @programStart = 
-      # @programEnd = 
-      # @uni = 
-      # @dept = 
+      @settings_start = Setting.where(name: "Application Start").first
+      @settings_deadline = Setting.where(name: "Application Deadline").first
+      @settings_not_date = Setting.where(name: "Notification Date").first
+      @settings_prog_start = Setting.where(name: "Program Start Date").first
+      @settings_prog_end = Setting.where(name: "Program End Date").first
+      @settings_university = Setting.where(name: "University").first
+      @settings_dept = Setting.where(name: "Department").first
+      # @values = Setting.first(6)
 
     end
   end
